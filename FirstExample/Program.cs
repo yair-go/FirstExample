fa961495dce9deb07d244331101d1527e515bc60
yairJCT <irigo1@gmail.com>
2019-11-04 11:45:31 +0200

    Monday

--- FirstExample/Program.cs
+++ FirstExample/Program.cs
@@ -10,6 +10,28 @@ namespace FirstExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("");
+            //string st = Console.ReadLine();
+            //int num2;
+            //Int32.TryParse(st, out num2);
+
+            //try
+            //{
+            //    int num = Int32.Parse(st);
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine(e.Message);
+            //}
+            Student student = new Student();
+            student.Name = "Yair";
+            Console.WriteLine(student);
+            Student[] students = new Student[10];
+            int[,] Sc = new int[12,31];
+            students.
+
         }
     }
 }
---
+++ FirstExample/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstExample
+{
+    class Student
+    {
+        #region Fields
+        private string name;
+        private int id;
+        private DateTime birthday;
+        #endregion
+
+        #region Ctors
+        public Student(string name = "" , int id = 1, int year = 1970, int month = 1, int day = 1 )
+        {
+            this.name = name;
+            this.id = id;
+            this.birthday = new DateTime(year, month, day);
+        }
+
+        public Student(string name, int id, DateTime birthday )
+        {
+            this.name = name;
+            this.id = id;
+            this.birthday = birthday;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public int Id { get => id;}
+
+        public DateTime Birthday { get; }
+        public int Age
+        {
+            get { return DateTime.Now.Year - birthday.Year; }
+        }
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return Name + " " + Age;
+        }
+
+
+        #endregion
+    }
+}
